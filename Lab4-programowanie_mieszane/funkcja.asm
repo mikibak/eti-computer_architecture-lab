10f31c92d4c34665b857e98af42a8a0eede31918
mikibak <107919074+mikibak@users.noreply.github.com>
2022-12-03 19:53:28 +0100

    Programowanie mieszane 1
    Wskaźniki, wywoływanie funkcji z poziomu c, przekazywanie i zwracanie wartości

---
+++ Lab4-programowanie_mieszane/funkcja.asm
@@ -0,0 +1,18 @@
+.686
+.model flat
+public _odejmij_jeden
+.code
+_odejmij_jeden PROC
+push ebp ; zapisanie zawartoœci EBP na stosie
+mov ebp,esp ; kopiowanie zawartoœci ESP do EBP
+push ebx ; przechowanie zawartoœci rejestru EBX
+; wpisanie do rejestru EBX adresu zmiennej zdefiniowanej
+; w kodzie w jêzyku C
+mov ebx, [ebp+8]
+mov eax, [ebx] ; odczytanie wartoœci zmiennej
+dec dword PTR [eax]
+pop ebx
+pop ebp
+ret
+_odejmij_jeden ENDP
+END
