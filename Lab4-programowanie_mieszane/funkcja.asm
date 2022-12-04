f06f3651f5886654fd1a66eb2f115841e245e8b6
mikibak <107919074+mikibak@users.noreply.github.com>
2022-12-04 17:15:08 +0100

    Przygotowanie do lab 4

--- Lab4-programowanie_mieszane/funkcja.asm
+++ Lab4-programowanie_mieszane/funkcja.asm
@@ -1,18 +1,51 @@
 .686
 .model flat
-public _odejmij_jeden
+public _dot_product
+public _replace_below_zero
 .code
-_odejmij_jeden PROC
-push ebp ; zapisanie zawartoœci EBP na stosie
-mov ebp,esp ; kopiowanie zawartoœci ESP do EBP
-push ebx ; przechowanie zawartoœci rejestru EBX
-; wpisanie do rejestru EBX adresu zmiennej zdefiniowanej
-; w kodzie w jêzyku C
-mov ebx, [ebp+8]
-mov eax, [ebx] ; odczytanie wartoœci zmiennej
-dec dword PTR [eax]
-pop ebx
-pop ebp
-ret
-_odejmij_jeden ENDP
+_dot_product PROC
+	push ebp ; zapisanie zawartoœci EBP na stosie
+	mov ebp,esp ; kopiowanie zawartoœci ESP do EBP
+	push ebx ; przechowanie zawartoœci rejestru EBX
+	mov ebx, [ebp+8] ; adres tablicy tab1
+	mov esi, [ebp+12] ; adres tablicy tab2
+	mov ecx, [ebp+16] ; liczba elementów tablicy
+	mov edi, 0 ;iloczyn
+	dec ecx
+	; wpisanie kolejnego elementu tablicy 1 do rejestru EAX
+	ptl: mov eax, [ebx]
+		mov edx, 0
+		mul word ptr [esi]
+		add edi, eax
+		add ebx, 4 ; wyznaczenie adresu kolejnego elementu
+		add esi, 4 ; wyznaczenie adresu kolejnego elementu
+	loop ptl ; organizacja pêtli
+	mov eax, edi ;zwracana wartoœæ w rejesrze eax
+	pop ebx ; odtworzenie zawartoœci rejestrów
+	pop ebp
+	ret ; powrót do programu g³ównego
+_dot_product ENDP
+
+_replace_below_zero PROC
+	push ebp ; zapisanie zawartoœci EBP na stosie
+	mov ebp,esp ; kopiowanie zawartoœci ESP do EBP
+	push ebx ; przechowanie zawartoœci rejestru EBX
+	mov esi, [ebp+8] ; adres tablicy tab
+	mov ecx, [ebp+12] ; liczba elementów tablicy
+	mov edi, [ebp+16] ; liczba na któr¹ bêdzie zamieniane
+
+	;dec ecx
+	; wpisanie kolejnego elementu tablicy do rejestru EAX
+	ptl: mov eax, [esi]
+		cmp eax, 0
+		jge nie_zamieniaj
+		mov [esi], edi
+		nie_zamieniaj:
+		add esi, 4 ; wyznaczenie adresu kolejnego elementu
+	loop ptl ; organizacja pêtli
+	mov eax, esi ;adres tablicy
+	pop ebx ; odtworzenie zawartoœci rejestrów
+	pop ebp
+	ret ; powrót do programu g³ównego
+_replace_below_zero ENDP
 END
--- Lab4-programowanie_mieszane/program.c
+++ Lab4-programowanie_mieszane/program.c
@@ -1,13 +1,52 @@
 #include <stdio.h>
-void odejmij_jeden(int** liczba);
+int dot_product(int tab1[], int tab2[], int n);
+int* replace_below_zero(int tab[], int n, int value);
+
+int write_array(int* tab1, int size) {
+	printf("[ ");
+	for (int i = 0; i < size; i++) {
+		printf("%d ", tab1[i]);
+	}
+	printf("]\n");
+}
+
 int main()
 {
-	int k;
-	int* wsk;
-	wsk = &k;
-	printf("\nProsze napisac liczbe: ");
-	scanf_s("%d", &k, 12);
-	odejmij_jeden(&wsk);
-	printf("\nWynik = %d\n", k);
+	int* tab1 = malloc(5 * sizeof(int));
+	int* tab2 = malloc(5 * sizeof(int));
+	
+	/*
+	char c;
+	printf("\nProsze napisac 5 liczb tab1: ");
+	for (int i = 0; i < 5; i++) {
+		scanf_s("%d%c", &tab1[i], &c, 12);
+		if (c == 10) break;
+	}
+	printf("\nProsze napisac 5 liczb tab2: ");
+	for (int i = 0; i < 5; i++) {
+		scanf_s("%d%c", &tab2[i], &c, 12);
+		if (c == 10) break;
+	}
+	*/
+
+	//filling tab1 and tab2
+	int j = 4;
+	for (int i = 0; i < 5; i++) {
+		tab1[i] = i-10;
+		tab2[i] = j;
+		j--;
+	}
+
+	write_array(tab1, 5);
+	write_array(tab2, 5);
+	printf("\n");
+
+	replace_below_zero(tab1, 5, 10);
+	int iloczyn_skalarny = dot_product(tab1, tab2, 5);
+
+	write_array(tab1, 5);
+	write_array(tab2, 5);
+	printf("Iloczyn skalarny: %d", iloczyn_skalarny);
+
 	return 0;
 }
