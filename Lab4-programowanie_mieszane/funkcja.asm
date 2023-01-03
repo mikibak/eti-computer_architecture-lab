3560801146d8a44354ca7ded43e5fc7d5e895a58
mikibak <107919074+mikibak@users.noreply.github.com>
2023-01-03 16:20:05 +0100

    Przygotowanie do lab 5, podstawy

--- Lab4-programowanie_mieszane/funkcja.asm
+++ Lab4-programowanie_mieszane/funkcja.asm
@@ -1,18 +1,16 @@
 .686
 .model flat
-public _dot_product
-public _replace_below_zero
-public _mse_loss
-public _merge_reversed
+public _srednia_harm
 
 .data
 
-	pol_n dd ?
-	tablica dd 32 dup (?)
+	tab1 dd 32 dup (?)
+	n dd ?
+	jeden dd 1.0
 
 .code
 
-_merge_reversed PROC
+_srednia_harm PROC
 	push ebp ; zapisanie zawartoœci EBP na stosie
 	mov ebp,esp ; kopiowanie zawartoœci ESP do EBP
 	push ebx ; przechowanie zawartoœci rejestru EBX
@@ -20,160 +18,32 @@ _merge_reversed PROC
 	push edi ; przechowanie zawartoœci rejestru EDI
 
 	mov esi, [ebp+8] ; adres tablicy tab1
-	mov ecx, [ebp+16] ; liczba elementów tablicy
-	mov edi, offset tablica ;adres w tablicy z³¹czonej
-
-	;sprawdzenie czy size < 32
-	cmp ecx, 32
-	ja zly_rozmiar
-
-	; wpisanie kolejnego elementu tablicy 1 do rejestru EAX
-	ptl: mov eax, [esi]
-		mov [edi], eax
-		add esi, 4 ; wyznaczenie adresu kolejnego elementu w tab1
-		add edi, 4 ; wyznaczenie adresu kolejnego elementu w tablicy
-	loop ptl ; organizacja pêtli
-
-	;obliczenie liczby bajtów, w³o¿enie do ebx
-	mov ecx, [ebp+16] ; liczba elementów tablicy
-	mov eax, ecx
-	mov edx, 0
-	mov esi, 8
-	mul esi
-	mov ebx, eax
-
-	mov esi, [ebp+12] ; adres tablicy tab1
-	
-	mov eax, offset tablica
-	add eax, ebx ;koniec tablicy
-	mov edi, eax
-	ptl2: sub edi, 4 ; wyznaczenie adresu kolejnego elementu w tablicy
-		mov eax, [esi]
-		mov [edi], eax
-		add esi, 4 ; wyznaczenie adresu kolejnego elementu w tab1
-	loop ptl2 ; organizacja pêtli2
-
-	mov eax, offset tablica
-
-	koniec:
-	pop edi ; odtworzenie zawartoœci rejestrów
-	pop esi
-	pop ebx
-	pop ebp
-	ret ; powrót do programu g³ównego
-
-	zly_rozmiar:
-		mov eax, 0
-		jmp koniec
-
-_merge_reversed ENDP
-
-
-_mse_loss PROC
-	push ebp ; zapisanie zawartoœci EBP na stosie
-	mov ebp,esp ; kopiowanie zawartoœci ESP do EBP
-	push ebx ; przechowanie zawartoœci rejestru EBX
-	push esi ; przechowanie zawartoœci rejestru ESI
-	push edi ; przechowanie zawartoœci rejestru EDI
-
-	mov ebx, [ebp+8] ; adres tablicy tab1
-	mov esi, [ebp+12] ; adres tablicy tab2
-	mov ecx, [ebp+16] ; liczba elementów tablicy
-
-	mov eax, ecx
-	mov edx, 0
-	mov edi, 2
-	div edi ;liczbê elementów dzielê przez 2
-
-	mov eax, edx ;reszta do eax
-	mov edx, 0 
-	mul edi 
-	mov pol_n, eax
-	mov edi, 0 ;suma
-	;dec ecx
-	; wpisanie kolejnego elementu tablicy 1 do rejestru EAX
-	ptl: mov eax, [ebx]
-		mov edx, 0
-		;mul word ptr [esi]
-		sub eax, [esi] ;odejmowanie y2
-		mul eax ;kwadrat
-		add edi, eax
-		add ebx, 4 ; wyznaczenie adresu kolejnego elementu
-		add esi, 4 ; wyznaczenie adresu kolejnego elementu
-	loop ptl ; organizacja pêtli
-
-	mov eax, edi ;suma kwadratów do eax w celu podzielenia
-	mov edx, 0
-	div dword ptr [ebp+16] ;dzielenie przez sumê
-	; 
-	mov esi, eax ;calosc z dziel przez n
-	mov edi, edx ;reszta z dziel przez n
-
-	mov eax, [ebp+16]
-	mov edx, 0
-	mov ecx, 2
-	div ecx
-	cmp eax, edi
-	jbe w_gore
-
-
-	koniec:
-	mov eax, esi
-	pop edi ; odtworzenie zawartoœci rejestrów
-	pop esi
-	pop ebx
-	pop ebp
-	ret ; powrót do programu g³ównego
-
-	w_gore:
-		inc esi
-		jmp koniec
-_mse_loss ENDP
-
-
-_dot_product PROC
-	push ebp ; zapisanie zawartoœci EBP na stosie
-	mov ebp,esp ; kopiowanie zawartoœci ESP do EBP
-	push ebx ; przechowanie zawartoœci rejestru EBX
-	mov ebx, [ebp+8] ; adres tablicy tab1
-	mov esi, [ebp+12] ; adres tablicy tab2
-	mov ecx, [ebp+16] ; liczba elementów tablicy
-	mov edi, 0 ;iloczyn
-	dec ecx
-	; wpisanie kolejnego elementu tablicy 1 do rejestru EAX
-	ptl: mov eax, [ebx]
-		mov edx, 0
-		mul word ptr [esi]
-		add edi, eax
-		add ebx, 4 ; wyznaczenie adresu kolejnego elementu
-		add esi, 4 ; wyznaczenie adresu kolejnego elementu
-	loop ptl ; organizacja pêtli
-	mov eax, edi ;zwracana wartoœæ w rejesrze eax
-	pop ebx ; odtworzenie zawartoœci rejestrów
-	pop ebp
-	ret ; powrót do programu g³ównego
-_dot_product ENDP
-
-_replace_below_zero PROC
-	push ebp ; zapisanie zawartoœci EBP na stosie
-	mov ebp,esp ; kopiowanie zawartoœci ESP do EBP
-	push ebx ; przechowanie zawartoœci rejestru EBX
-	mov esi, [ebp+8] ; adres tablicy tab
 	mov ecx, [ebp+12] ; liczba elementów tablicy
-	mov edi, [ebp+16] ; liczba na któr¹ bêdzie zamieniane
+	mov ebx, 0 ; indeks w tab1
+	mov n, ecx
+	mov tab1, esi
 
-	;dec ecx
-	; wpisanie kolejnego elementu tablicy do rejestru EAX
-	ptl: mov eax, [esi]
-		cmp eax, 0
-		jge nie_zamieniaj
-		mov [esi], edi
-		nie_zamieniaj:
-		add esi, 4 ; wyznaczenie adresu kolejnego elementu
-	loop ptl ; organizacja pêtli
-	mov eax, esi ;adres tablicy
-	pop ebx ; odtworzenie zawartoœci rejestrów
+	finit
+	fldz ; ST(0): suma
+
+	petla:
+		fld jeden ; ST(0): 1  ST(1): suma
+		fld dword ptr [esi+ebx] ;ST(0): element tablicy; ST(1): 1; ST(2): suma
+		fdivp ;ST(0): 1/element tablicy  ST(1): suma
+		faddp ;ST(0): suma
+		add ebx, 4
+	loop petla
+
+	fild n ;ST(0): n; ST(1): suma
+	fxch ST(1) ;ST(0): suma; ST(1): n
+	fdivp ;ST(0): n/suma
+
+	pop edi ; odtworzenie zawartoœci rejestrów
+	pop esi
+	pop ebx
 	pop ebp
 	ret ; powrót do programu g³ównego
-_replace_below_zero ENDP
+
+_srednia_harm ENDP
+
 END
--- Lab4-programowanie_mieszane/program.c
+++ Lab4-programowanie_mieszane/program.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-//int dot_product(int tab1[], int tab2[], int n);
-//int* replace_below_zero(int tab[], int n, int value);
-int mse_loss(int tab1[], int tab2[], int n);
-int* merge_reversed(int tab1[], int tab2[], int n);
 
-int write_array(int* tab, int size) {
+float srednia_harm(float* tablica, unsigned int n);
+float nowy_exp(float x);
+
+int write_array(float* tab, int size) {
 	printf("[ ");
 	for (int i = 0; i < size; i++) {
-		printf("%d ", tab[i]);
+		printf("%f ", tab[i]);
 	}
 	printf("]\n");
 }
@@ -16,54 +15,23 @@ int main()
 {
 	int size;
 	
-	
 	printf("\nProsze wpisaæ rozmiar: ");
 	scanf_s("%d", &size, 12);
 
-	int* tab1 = malloc(size * sizeof(int));
-	int* tab2 = malloc(size * sizeof(int));
-	int* tablica_zlaczona = malloc(size * 2 *sizeof(int));
+	float* tab1 = malloc(size * sizeof(float));
 
 	char c;
 	printf("\nProsze napisac %d liczb tab1: ", size);
 	for (int i = 0; i < 5; i++) {
-		scanf_s("%d%c", &tab1[i], &c, 12);
-		if (c == 10) break;
-	}
-	printf("\nProsze napisac %d liczb tab2: ", size);
-	for (int i = 0; i < 5; i++) {
-		scanf_s("%d%c", &tab2[i], &c, 12);
+		scanf_s("%f%c", &tab1[i], &c, 12);
 		if (c == 10) break;
 	}
 	
-	int wynik = 0;
-	wynik = mse_loss(tab1, tab2, size);
-
-	//filling tab1 and tab2
-	/*
-	int j = 4;
-	for (int i = 0; i < 5; i++) {
-		tab1[i] = i-10;
-		tab2[i] = j;
-		j--;
-	}
-	*/
-
-	/*
-	write_array(tab1, 5);
-	write_array(tab2, 5);
-	printf("\n");
-	
-	replace_below_zero(tab1, 5, 10);
-	int iloczyn_skalarny = dot_product(tab1, tab2, 5);
-	*/
-
-	tablica_zlaczona = merge_reversed(tab1, tab2, size);
+	float wynik = 0;
+	wynik = srednia_harm(tab1, size);
 
 	write_array(tab1, size);
-	write_array(tab2, size);
-	write_array(tablica_zlaczona, size*2);
-	printf("Wynik: %d", wynik);
+	printf("Wynik: %f", wynik);
 
 	return 0;
 }
